7e38b830ac2d5164fa1303a15b42faf67d1810f0
NHBKhang <109735934+NHBKhang@users.noreply.github.com>
2023-04-13 17:00:11 +0700

    Add files via upload

---
+++ Lottery/Lottery(INCOMPL)/Congrat.Designer.cs
@@ -0,0 +1,110 @@
+﻿
+namespace Lottery
+{
+    partial class FCongrat
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.lbGiai = new System.Windows.Forms.Label();
+            this.lbMoney = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 19.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.Red;
+            this.label1.Location = new System.Drawing.Point(0, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(487, 60);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Chúc mừng bạn";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // lbGiai
+            // 
+            this.lbGiai.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbGiai.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(64)))), ((int)(((byte)(0)))));
+            this.lbGiai.Location = new System.Drawing.Point(117, 79);
+            this.lbGiai.Name = "lbGiai";
+            this.lbGiai.Size = new System.Drawing.Size(253, 37);
+            this.lbGiai.TabIndex = 1;
+            this.lbGiai.Text = "Xổ số ";
+            this.lbGiai.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // lbMoney
+            // 
+            this.lbMoney.Font = new System.Drawing.Font("Microsoft Sans Serif", 25.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbMoney.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.lbMoney.Location = new System.Drawing.Point(36, 127);
+            this.lbMoney.Name = "lbMoney";
+            this.lbMoney.Size = new System.Drawing.Size(430, 57);
+            this.lbMoney.TabIndex = 1;
+            this.lbMoney.Text = "0";
+            this.lbMoney.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label2
+            // 
+            this.label2.BackColor = System.Drawing.Color.Red;
+            this.label2.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.label2.ForeColor = System.Drawing.Color.White;
+            this.label2.Location = new System.Drawing.Point(0, 244);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(487, 42);
+            this.label2.TabIndex = 2;
+            this.label2.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // FCongrat
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoSize = true;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(255)))), ((int)(((byte)(192)))));
+            this.ClientSize = new System.Drawing.Size(487, 286);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.lbMoney);
+            this.Controls.Add(this.lbGiai);
+            this.Controls.Add(this.label1);
+            this.MaximizeBox = false;
+            this.Name = "FCongrat";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Chúc mừng";
+            this.Load += new System.EventHandler(this.FCongrat_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label lbGiai;
+        private System.Windows.Forms.Label lbMoney;
+        private System.Windows.Forms.Label label2;
+    }
+}
---
+++ Lottery/Lottery(INCOMPL)/Scripts/GiaiThuong.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lottery
+{
+    public enum Giai
+    {
+        GiaiBa,
+        GiaiNhi,
+        GiaiNhat,
+        GiaiDacBiet,
+        KhongGiai
+    }
+    static class GiaiThuong
+    {
+
+        private static string[] tenGiai = {"giải ba", "giải nhì", "giải nhất", "giải đặc biệt"};
+        private static long[] tienThuong = {100000, 500000, 1000000, 10000000};
+        
+        public static long TienThuong(int viTri)
+        {
+            return tienThuong[viTri];
+        }
+        public static void ResetTienThuong()
+        {
+            tienThuong[(int)Giai.GiaiDacBiet] = 10000000;
+        }
+        public static void inc()
+        {
+            tienThuong[(int) Giai.GiaiDacBiet] += 100000;
+        }
+        public static string FormatPrice(long tienThuong)
+        {
+            return String.Format("{0:N0}đ", tienThuong);
+        }
+        public static string GetTenGiai(Giai giai)
+        {
+            return tenGiai[(int)giai];
+        }
+        public static long GetTienThuongGiai(Giai giai)
+        {
+            return tienThuong[(int)giai];
+        }
+    }
+}
